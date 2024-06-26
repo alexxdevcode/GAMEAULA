91833ed2f16194a31655dfc72566505fe83280a5
asantos <allhollys@gmail.com>
2024-06-26 16:12:45 -0300

    AlexSantos

---
+++ Model/Background.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Background:
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
---
+++ Model/Entify.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entify:
+    def __init__(self):
+        self.Attribute1 = None
+        self.Attribute2 = None
+        self.Attribute = None
+
